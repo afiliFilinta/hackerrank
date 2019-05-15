13f8dd2739084dcb39ee60f7d555aef056a6e266
mkarsli <mkarsli@netas.com.tr>
2019-05-15 11:20:04 +0300

    RansomNote solution is added

--- src/hackerrank/ipk/dict/RansomNote.java
+++ src/hackerrank/ipk/dict/RansomNote.java
@@ -5,10 +5,40 @@
  */
 package hackerrank.ipk.dict;
 
+import java.util.Hashtable;
+import java.util.Iterator;
+
 /**
  *
  * @author mkarsli
  */
 public class RansomNote {
-    
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(String[] magazine, String[] note) {
+
+        Hashtable<String, Integer> table = new Hashtable<String, Integer>();
+        for (String m : magazine) {
+            if (table.containsKey(m)) {
+                table.put(m, table.get(m) + 1);
+            } else {
+                table.put(m, 1);
+            }
+        }
+
+        for (String n : note) {
+            if (!table.containsKey(n)) {
+                System.out.println("No");
+                return;
+            }
+
+            int counter = table.get(n) - 1;
+            if (counter == 0) {
+                table.remove(n);
+            } else {
+                table.put(n, counter);
+            }
+        }
+        System.out.println("Yes");
+    }
 }
